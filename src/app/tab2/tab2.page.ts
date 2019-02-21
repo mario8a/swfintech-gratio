8e47094d7161e05fd9a36c41391a3ddac27906c9
Mario Ochoa <mario8ato@hotmail.com>
2019-02-20 21:35:06 -0600

    actualizacion del qr

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,262 +9,258 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab2Page {
 
-  qrData = null;
-  createdCode = null;
-  scannedCode = null;
-
-  // qrData = "0"; //Para integrar al createdCode
-  // createdCode = null; //Crea el QR
+  qrData = "0"; //Para integrar al createdCode
+  createdCode = null; //Crea el QR
 
   //Variables JSON QR
-  // jsonQRot = "0001";
-  // jsonQRalias = "Alias";
-  // jsonQRcl = "012180004585820842";
-  // jsonQRtype = "CL";
-  // jsonQRrefn = "0";
-  // jsonQRrefa = "MARIO";
-  // jsonQRamount = "0";
-  // jsonQRbank = "00012";
-  // jsonQRcountry = "MX";
-  // jsonQRcurrency = "MXN";
+  jsonQRot = "0001";
+  jsonQRalias = "Alias";
+  jsonQRcl = "012180004585820842";
+  jsonQRtype = "CL";
+  jsonQRrefn = "0";
+  jsonQRrefa = "MARIO";
+  jsonQRamount = "0";
+  jsonQRbank = "00012";
+  jsonQRcountry = "MX";
+  jsonQRcurrency = "MXN";
   //Fin de Variable JSON QR
 
 
   //JSON QR
-  // obj = {
-  //   "ot": this.jsonQRot,
-  //   "dOp":[
-  //     {"alias": this.jsonQRalias},
-  //     {"cl": this.jsonQRcl},
-  //     {"type": this.jsonQRtype},
-  //     {"refn": this.jsonQRrefn},
-  //     {"refa": this.jsonQRrefa},
-  //     {"amount": this.qrData},
-  //     {"bank": this.jsonQRbank},
-  //     {"country": this.jsonQRcountry},
-  //     {"currency": this.jsonQRcurrency}
-  //   ]
-  // };
-  // qrDataJson = JSON.stringify(this.obj);
+  obj = {
+    "ot": this.jsonQRot,
+    "dOp":[
+      {"alias": this.jsonQRalias},
+      {"cl": this.jsonQRcl},
+      {"type": this.jsonQRtype},
+      {"refn": this.jsonQRrefn},
+      {"refa": this.jsonQRrefa},
+      {"amount": this.qrData},
+      {"bank": this.jsonQRbank},
+      {"country": this.jsonQRcountry},
+      {"currency": this.jsonQRcurrency}
+    ]
+  };
+  qrDataJson = JSON.stringify(this.obj);
   //Fin de JSON QR
 
 
-  // qrURL = this.qrDataJson;
-  // User = "000001/";
+  qrURL = this.qrDataJson;
+  User = "000001/";
 
-  // qrCode = this.qrURL.concat(this.User);
+  qrCode = this.qrURL.concat(this.User);
 
   constructor(private barcodeScanner: BarcodeScanner, public alertController: AlertController) {}
 
-  async createCode() {
+  createCode() {
     this.createdCode = this.qrData;
   }
 
-  // scanCode() {
-  //   this.barcodeScanner.scan().then(barcodeData => {
-  //     this.scannedCode = barcodeData.text;
-  //   }, (err) => {
-  //       console.log('Error: ', err);
-  //   });
-  // }
+  scanCode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.createdCode = barcodeData.text;
+    }, (err) => {
+        console.log('Error: ', err);
+    });
+  }
 
     //Control de Págo
-    // oneButton()
-    // {
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode)//Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "1"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "1"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    // }
+    oneButton()
+    {
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode)//Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "1"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "1"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+    }
 
-    // twoButton()
-    // {
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "2"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "2"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    // }
+    twoButton()
+    {
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "2"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "2"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+    }
 
-    // threeButton()
-    // {
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "3"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "3"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    // }
+    threeButton()
+    {
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "3"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "3"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+    }
 
-    // fourButton(){
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "4"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "4"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    // }
+    fourButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "4"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "4"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+    }
 
-    // fiveButton(){
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "5"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "5"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    // }
+    fiveButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "5"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "5"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+    }
 
-    // sixButton(){
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "6"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "6"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    // }
+    sixButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "6"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "6"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+    }
 
-    // sevenButton(){
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "7"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "7"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    // }
+    sevenButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "7"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "7"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+    }
 
-    // eightButton(){
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "8"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "8"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    //   }
+    eightButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "8"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "8"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
     
-    //   nineButton(){
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "9"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "9"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    //   }
+      nineButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "9"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "9"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
     
-    //   zeroButton(){
-    //   if (this.qrData.length > 4){
-    //     this.createdCode = this.qrData.slice(0,5);
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
-    //   else{
-    //     if (this.qrData == "0"){
-    //       this.qrData = "0"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //     else{
-    //       this.qrData = this.qrData + "0"
-    //       this.createdCode = this.qrData;
-    //       this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //     }
-    //   }
-    //   }
+      zeroButton(){
+      if (this.qrData.length > 4){
+        this.createdCode = this.qrData.slice(0,5);
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
+      else{
+        if (this.qrData == "0"){
+          this.qrData = "0"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+        else{
+          this.qrData = this.qrData + "0"
+          this.createdCode = this.qrData;
+          this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+        }
+      }
+      }
     
-    //   cleanButton(){
-    //     this.qrData = "0"
-    //     this.createdCode = this.qrData;
-    //     this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
-    //   }
+      cleanButton(){
+        this.qrData = "0"
+        this.createdCode = this.qrData;
+        this.qrCode = this.qrURL.concat(this.createdCode) //Crea un QR Completo
+      }
 }
